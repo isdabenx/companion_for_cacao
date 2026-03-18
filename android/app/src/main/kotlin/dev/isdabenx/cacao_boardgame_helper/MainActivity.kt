7650defccdf984e455190458948822b9f71ca084
isdabenx <blluch@gmail.com>
2026-03-18 01:51:34 +0100

    fix: :bug: fix Android package name in build.gradle and move MainActivity
    - Update namespace and applicationId to dev.isdabenx.companion_for_cacao in build.gradle (Groovy)
    - Remove redundant build.gradle.kts that was being ignored by Gradle
    - Move MainActivity.kt to correct package directory companion_for_cacao

--- android/app/src/main/kotlin/dev/isdabenx/cacao_boardgame_helper/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.isdabenx.cacao_boardgame_helper
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/dev/isdabenx/companion_for_cacao/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.isdabenx.companion_for_cacao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
